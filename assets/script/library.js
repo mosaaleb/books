dd3d1e0f6a8b6ad929a656947459fdb8fb58e579
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-08-05 15:18:00 +0200

    Add local storage getBooks and addBooks

--- assets/script/library.js
+++ assets/script/library.js
@@ -13,9 +13,6 @@ function Book(title, pagesNumber, author) {
     this.isRead = false;
 }
 
-// Book.prototype = { 
-//     isRead: false
-// }
 
 function addBookToLibrary() {
     let title = document.getElementById("name").value;
@@ -24,47 +21,30 @@ function addBookToLibrary() {
 
     let book = new Book(title, author, pages);
     myLibrary.push(book);
+    addBook(book);
 }
 
-function render() {
-    let book1 = new Book('title1', 'author1', 200);
-    let book2 = new Book('title2', 'author2', 200);
-    let book3 = new Book('title3', 'author3', 200);
-    
+function displayBooks() {
+    let books = getBooks();
 
-    myLibrary.push(book1);
-    myLibrary.push(book2);
-    myLibrary.push(book3);
+    const table = document.getElementById("books");
 
-    const div = document.createElement('table');
-    div.className = 'books';
-    
-    div.innerHTML = `
-    <tr>
-        <th>Book name</th>
-        <th>Book author</th>
-        <th>Book pages numbers</th>
-        <th>Delete button</th>
-        <th>Read status</th>
-    </tr>
-    `
-    
-    myLibrary.forEach(function(book,index) {
-        div.innerHTML += `
-            <tr>
-                <td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.pagesNumber}</td>
-                <td> <button class = "book_delete_button" data-index="${index}">Delete</button> </td>
-                <td> <button class = "book_read_button" data-index="${index}">${book.isRead ? "unread" : "read"}</button> </td>
-            </tr>
+    books.forEach(function(book,index) {
+        let rowBook = document.createElement("tr");
+        
+        rowBook.innerHTML += `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pagesNumber}</td>
+            <td> <button class = "book_delete_button" data-index="${index}">Delete</button> </td>
+            <td> <button class = "book_read_button" data-index="${index}">${book.isRead ? "unread" : "read"}</button> </td>
         `;
+        table.appendChild(rowBook);
     });
 
-    document.body.appendChild(div);
 }
 
-render();
+displayBooks();
 
 let bookDeleteButtons = document.querySelectorAll(".book_delete_button");
 let bookReadButtons = document.querySelectorAll(".book_read_button");
@@ -91,13 +71,38 @@ bookReadButtons.forEach((button) => {
 })
 
 function deleteBook() {
+    let books = getBooks();
     let index = event.target.dataset.index;
-    myLibrary.splice(index, 1);
-    console.log(myLibrary);
+    books.splice(index, 1);
+    // console.log(myLibrary);
+    localStorage.books = JSON.stringify(books);
+    document.location.reload();
 }
 
 function readBookToggle() {
     let index = event.target.dataset.index;
     myLibrary[index].isRead = !myLibrary[index].isRead;
     console.log(myLibrary);
+}
+
+
+// Local storage 
+
+function getBooks() {
+    let books = [];
+
+    if (localStorage.getItem("books") === null) {
+        localStorage.books = JSON.stringify(books);
+    } else {
+        books = JSON.parse(localStorage.books);
+    }
+
+    return books;
+}
+
+
+function addBook(book) {
+    let books = getBooks();
+    books.push(book);
+    localStorage.books = JSON.stringify(books);
 }
