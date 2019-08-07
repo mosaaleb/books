e1098625643f459fffebeba82ceacc2c3ef686c6
daashandayo <dj30c.1@gmail.com>
2019-08-07 15:25:15 +0530

    Fix for re-review

--- assets/script/library.js
+++ assets/script/library.js
@@ -1,46 +1,42 @@
-"use strict";
-
-let myLibrary = [];
-const addNewBookButton = document.getElementById("add_new_book_button");
-const addBookForm = document.getElementById("newBook");
-
-addBookForm.style.display = "none";
-
-function Book(title, pagesNumber, author) {
-    this.title = title;
-    this.pagesNumber = pagesNumber
-    this.author = author;
-    this.isRead = false;
-}
-
-function addBookToLibrary() {
-    const title = document.getElementById("name").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-
-    // check for validation
-    if (title == "" || author == "" || pages == "" ) {
-        showMessage("You need to fill all the fields", "error");
-        event.preventDefault();
-    } else {
-        const book = new Book(title, pages, author);
-        myLibrary.push(book);
-        addBook(book);
-        // showMessage("You need to fill all the fields", "error");
-
+//--------------------------CLASS CODE-----------------------//
+class Book {
+    constructor(title, author, pagesNumber, isRead = false) {
+      this.title = title;
+      this.author = author;
+      this.pagesNumber = pagesNumber;
+      this.isRead = isRead; 
     }
-
-
-}
-
-function displayBooks() {
-    const books = getBooks();
-
-    const table = document.getElementById("books");
-
-    let rowBook;
-
-    books.forEach(function(book,index) {
+    toggleRead() {
+      this.isRead = !this.isRead;
+    }
+    info() {
+    let info = `${this.title} by ${this.author}, ${this.pagesNumber} pages, `;
+    return (info + (this.isRead ? "read" : "not read yet"));
+    }
+  }
+  
+  class Library {
+    constructor(books) {
+      // `books` is an Array
+      this.books = books;
+    }
+    addBook(book) {
+      this.books.push(book)
+    }
+    removeBook(index) {
+      this.books.splice(index,1)
+    }
+  }
+  
+  //---------------------------DOM CODE-----------------------//
+  class UserInterface {
+    static renderBooks(books) {
+      const table = document.getElementById("books");
+      while (table.childElementCount > 1 ) {
+        table.removeChild(table.lastChild);
+      }
+      let rowBook;
+      books.forEach(function(book, index) {
         rowBook = document.createElement("tr");
         
         rowBook.innerHTML += `
@@ -50,81 +46,114 @@ function displayBooks() {
             <td> <button class = "btn btn-danger book_delete_button" data-index="${index}">Delete</button> </td>
             <td> <button class = "btn btn-info book_read_button" data-index="${index}">${book.isRead ? "unread" : "read"}</button> </td>
         `;
+        
         table.appendChild(rowBook);
+  
+        // ADD EVENT LISTENERS
+        const bookDeleteButtons = document.querySelectorAll(".book_delete_button");
+        const bookReadButtons = document.querySelectorAll(".book_read_button");
+        bookDeleteButtons.forEach((button)=>{
+          button.addEventListener("click", deleteBook);
+        })
+      
+        bookReadButtons.forEach((button) => {
+          button.addEventListener("click", readToggle);
+        })
     });
-
-}
-
-displayBooks();
-
-const bookDeleteButtons = document.querySelectorAll(".book_delete_button");
-const bookReadButtons = document.querySelectorAll(".book_read_button");
-
-addNewBookButton.addEventListener( "click", renderAddBookForm );
-
-function renderAddBookForm() {
-    
-    const displayStatus  = addBookForm.style.display
-
-    if (displayStatus == "none") {
-        addBookForm.style.display = "block";
-    } else {
-        addBookForm.style.display = "none";
     }
-}
-
-bookDeleteButtons.forEach((button)=>{
-    button.addEventListener("click", deleteBook);
-})
-
-bookReadButtons.forEach((button) => {
-    button.addEventListener("click", readBookToggle);
-})
-
-function deleteBook() {
-    const books = getBooks();
-    const index = event.target.dataset.index;
-    books.splice(index, 1);
-    // console.log(myLibrary);
-    localStorage.books = JSON.stringify(books);
-    document.location.reload();
-}
-
-function readBookToggle() {
-    const books = getBooks();
-    const index = event.target.dataset.index;
-    books[index].isRead = !books[index].isRead;
-    localStorage.books = JSON.stringify(books);
-    document.location.reload();
-    console.log(myLibrary);
-}
-
-
-// Local storage 
-
-function getBooks() {
-    let books = [];
-
-    if (localStorage.getItem("books") === null) {
-        localStorage.books = JSON.stringify(books);
-    } else {
-        books = JSON.parse(localStorage.books);
+    static AddBookToggle() {
+      const addBookForm = document.getElementById("newBook");
+      const displayStatus  = addBookForm.style.display
+      if (displayStatus == "none") {
+          addBookForm.style.display = "block";
+      } else {
+          addBookForm.style.display = "none";
+      }
     }
-
-    return books;
-}
-
-
-function addBook(book) {
-    const books = getBooks();
-    books.push(book);
+  }
+  
+  //---------------------LOCALSTORAGE CODE--------------------//
+  
+  // SERIALIZE to localStorage
+  function serialize(books) {
     localStorage.books = JSON.stringify(books);
-}
-
-// messages 
-function showMessage(message, className) {
+  }
+  // DESERIALIZE from localStorage
+  function deserialize() {
+    if ( localStorage.books == undefined ) {
+      const books = [];
+      localStorage.books = JSON.stringify(books);
+    } else {
+      const raw = JSON.parse(localStorage.books);
+      const books = raw.map(book => new Book(book.title, book.author,book.pagesNumber, book.isRead));
+      const library = new Library(books);
+      return library;
+    }
+  }
+  
+  //-----------------------CONTROLLER CODE--------------------//
+  function main() {
+    UserInterface.AddBookToggle();
+    const addBook = document.getElementById("add_new_book_button")
+    addBook.addEventListener("click", UserInterface.AddBookToggle);
+  
+    const library = deserialize();
+    UserInterface.renderBooks(library.books);  
+  }
+  function deleteBook() {
+    const index = event.target.dataset.index;
+    // HANDLE LOCALSTORAGE
+    const library = deserialize();
+    library.removeBook(index);
+    serialize(library.books);
+  
+    // HANDLE UI
+    UserInterface.renderBooks(library.books);
+  }
+  function readToggle() {
+    const index = event.target.dataset.index;
+    // HANDLE LOCALSTORAGE
+    const library = deserialize();
+    library.books[index].toggleRead();
+    serialize(library.books);
+  
+    // HANDLE UI
+    UserInterface.renderBooks(library.books);
+  }
+  function addBookToLibrary() {
+    event.preventDefault();
+  
+    // Get values from form
+    const title = document.getElementById("name").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+  
+    // check for validation
+    if (title == "" || author == "" || pages == "" ) {
+      showMessage("You need to fill all the fields", "error");
+    } else {
+      // HANDLE LOCALSTORAGE
+      const book = new Book(title, author, pages);
+      const library = deserialize();
+      library.addBook(book);
+      serialize(library.books);
+  
+      // HANDLE UI
+      UserInterface.renderBooks(library.books);
+    }
+  
+    // Reset form fields
+    document.getElementById("name").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+  }
+  
+  function showMessage(message, className) {
     const messageContainer = document.createElement('div');
     messageContainer.className = "alert " + className;
     messageContainer.innerHTML = message;
     document.querySelector(".header").appendChild(messageContainer);
-}
+  }
+  
+  
+  main();
